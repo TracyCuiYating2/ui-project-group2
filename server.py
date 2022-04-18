f3227d0646a6a00cae5829c6fd8361844118fc0d
preduan <cd3136@barnard.edu>
2022-04-18 01:09:02 -0400

    added an image as data to server and added learn and quiz basic pages

--- server.py
+++ server.py
@@ -4,15 +4,38 @@ from flask import Response, request, jsonify
 from itsdangerous import json
 app = Flask(__name__)
 
+
+
+learnData =[
+    {
+        "id": 0,
+        "img": "https://b7d3d5f9.rocketcdn.me/wp-content/themes/olympus/utimages/ukuchords/ukuchords_howtoreadchordcharts.png",
+        "altText": "There's a single ukulele with some diagrams for where to place the fingers."
+    }
+]
+
+
+
+
 # ROUTES
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+
 @app.route('/learn/basic')
 def basic():
     return render_template('basic.html')
+
+@app.route('/learn/quiz')
+def quiz():
+    return render_template('quiz.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
